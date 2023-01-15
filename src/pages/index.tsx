5bfe44a70f69a49d8c80da4179bce3097e8c22b5
davud <davudgorqi@gmail.com>
2023-01-15 10:27:05 +0100

    chore: navbar takes whole screen when opened on mobile, navbar stuff and more responsiveness stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
@@ -11,7 +13,32 @@ const Index = () => {
         />
       }
     >
-      <div>KAR</div>
+      <section className="flex flex-col md:flex-row">
+        <div className="p-5 text-center md:p-14 md:text-start">
+          <div>
+            <span className="text-6xl font-semibold">Hello</span>
+          </div>
+          <div className="w-full text-lg text-gray-600 md:w-1/2">
+            <p className="">
+              this is a long paragraph heheslk spoifu posidfu klsjafposdiu
+              pofksjdapojsakofj spofiusod.
+            </p>
+          </div>
+          <div className="mt-14">
+            <button className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700">
+              Get Started
+            </button>
+          </div>
+        </div>
+        <div>
+          <Image
+            width={800}
+            height={1000}
+            src={'/assets/images/background.png'}
+            alt="Not Found"
+          />
+        </div>
+      </section>
     </Main>
   );
 };
--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,8 +1,9 @@
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from 'next/image';
 import Link from 'next/link';
 import type { ReactNode } from 'react';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 import { AppConfig } from '@/utils/AppConfig';
 
@@ -13,9 +14,12 @@ type IMainProps = {
 
 const Main = (props: IMainProps) => {
   const menu = useRef<any>();
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
   function handleMenuButtonClick() {
+    setNavbarOpen((prev) => !prev);
     menu.current.classList.toggle('hidden');
+    menu.current.classList.toggle('mt-24');
   }
 
   return (
@@ -23,9 +27,16 @@ const Main = (props: IMainProps) => {
       {props.meta}
 
       <div className="mx-auto">
-        <div className="flex justify-between border-b border-gray-300 p-7">
-          <span>Logo</span>
-          <div>
+        <div className="flex justify-between border-b border-gray-300 p-2">
+          <Image
+            className="mb-auto cursor-pointer object-contain"
+            src="/assets/images/logo.png"
+            height={75}
+            width={75}
+            alt="Not Found"
+            quality={100}
+          />
+          <div className="mt-6">
             <div className="flex w-full justify-end pr-7">
               <button
                 className="block h-6 w-6 text-xl md:hidden"
@@ -38,7 +49,7 @@ const Main = (props: IMainProps) => {
               ref={menu}
               className="hidden h-screen w-full flex-col text-end md:flex md:h-auto"
             >
-              <ul className="mt-12 mr-5 flex-wrap gap-5 text-xl md:mt-0 md:flex md:justify-end md:gap-10 lg:gap-20">
+              <ul className="mt-12 mr-5 flex flex-col flex-wrap gap-16 text-2xl md:mt-0 md:flex-row md:justify-end md:gap-10 md:text-xl lg:gap-20">
                 <li>
                   <Link
                     href="/"
@@ -68,7 +79,9 @@ const Main = (props: IMainProps) => {
           </div>
         </div>
 
-        <div className="content py-5 text-xl">{props.children}</div>
+        {!navbarOpen && (
+          <div className="content py-5 text-xl">{props.children}</div>
+        )}
 
         <div className="border-t border-gray-300 py-8 text-center text-sm">
           © Copyright {new Date().getFullYear()} {AppConfig.title}.
