564ccba6d25722cd22eb89e62f8147ed8b5e6cad
davud <davudgorqi@gmail.com>
2023-02-04 20:24:32 +0100

    feat: about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -6,7 +6,21 @@ const About = () => (
     meta={<Meta title="About us" description="About Programmer's University" />}
   >
     <section id="about-section">
-      <h1 className="text-black">coming soon...</h1>
+      <div className="about-container-container">
+        <div className="about-container flex flex-col justify-center py-8 px-24 drop-shadow-2xl">
+          <article className="-mt-24 flex flex-col justify-center text-center">
+            <h1 className="text-lg text-black md:text-3xl">About us</h1>
+            <p className="text-base text-black opacity-75 md:text-xl">
+              Hello this is a long text which is about the Programmers
+              University course, Hello this is a long text which is about the
+              Programmers University course, Hello this is a long text which is
+              about the Programmers University course, Hello this is a long text
+              which is about the Programmers University course, Hello this is a
+              long text which is about the Programmers University course
+            </p>
+          </article>
+        </div>
+      </div>
     </section>
   </Main>
 );
