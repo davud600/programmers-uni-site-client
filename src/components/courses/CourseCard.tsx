148389522ef03ccc18312b90988fd83acac4c70b
davud <davudgorqi@gmail.com>
2023-02-22 08:36:21 +0100

    fix: safari bug and small style tweaks

--- src/components/courses/CourseCard.tsx
+++ src/components/courses/CourseCard.tsx
@@ -4,9 +4,9 @@ const CourseCard = ({ course }: { course: Course }) => {
   return (
     <article
       // eslint-disable-next-line tailwindcss/no-custom-classname
-      className={`testing-card-safari course-card ${course.type}-course-card flex flex-col justify-center overflow-hidden py-4 px-8 text-center drop-shadow-2xl md:px-20`}
+      className={`card-safari course-card ${course.type}-course-card flex flex-col justify-center overflow-hidden py-4 px-8 text-center drop-shadow-2xl md:px-20`}
     >
-      <h2 className="text-base text-black md:text-lg">{course.title}</h2>
+      <h2 className="-mb-2 text-base text-black md:text-lg">{course.title}</h2>
       <p className="text-sm opacity-75 md:text-lg">{course.details}</p>
       <button className="text-base md:text-lg">More details</button>
     </article>
